a7e9578d370bd4d8bf1223a49d5b1adf2d06c716
Stressgorz <stressgor@gmail.com>
2024-03-06 00:19:11 +0800

    commit

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -3,9 +3,9 @@
   <!-- Sidebar - Brand -->
   <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('admin')}}">
     <div class="sidebar-brand-icon">
-    <img src="{{ asset('/images/logo.jpg') }}" id="slip" alt="..." style = 'max-width:60px;'>
+    <img src="{{ asset('/images/logo.png') }}" id="slip" alt="..." style = 'max-width:100px;'>
     </div>
-    <div class="sidebar-brand-text mx-3">Admin</div>
+
   </a>
 
   <!-- Divider -->
